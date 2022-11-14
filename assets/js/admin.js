ce83d2ea2703b180d9a393f1873f279efbbd001e
Stas Arshanski <stuk88@gmail.com>
2022-11-14 18:20:54 +0200

    try to update admin selectize for collections

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,12 +3,16 @@ $(document).ready(function(){
     $('[date-picker]').datetimepicker({format:'MM-DD-YYYY'});
     $('.selectize').each(function(elem) {
 
-        $(this).selectize({
+        let value = JSON.parse($(this).attr('value'));
+
+        let $selectized = $(this).selectize({
             labelField: 'title',
             searchField: ['title'],
             options: JSON.parse($(this).attr("data-options")),
             plugins: ['tree']
         });
+
+        $selectized[0].selectize.setValue(value);
     })
 
     $('.selectize_select').selectize({
